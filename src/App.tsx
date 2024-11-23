331cad2315dfb358048d9acd4bba9d7c3d272f87
SpitSmoke <jmfelicio.sp@gmail.com>
2024-11-23 02:35:11 -0300

    first part

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { GlobalStyle } from './styles/EstiloGlobal'
+import Header from './components/header'
+
+
+function App() {
+  return (
+    <>
+    <GlobalStyle />
+    <Header />
+    </>
+  )
+}
+
+export default App
